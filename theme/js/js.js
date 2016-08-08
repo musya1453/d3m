7081c9bd1ce86929db0b5651a6257ce6abee023a
Musaev Ruslan <musya.wow@gmail.com>
2016-08-08 10:38:01 +0300

    'slice

--- theme/js/js.js
+++ theme/js/js.js
@@ -1,25 +1,42 @@
 (function ($) {
 
-  if (typeof Drupal != 'undefined') {
-    Drupal.behaviors.wcportalMobileTheme = {
-      attach: function (context, settings) {
-        init();
-      },
-
-      completedCallback: function () {
-        // Do nothing. But it's here in case other modules/themes want to override it.
-      }
-    }
-  }
-
-  $(function () {
-    if (typeof Drupal == 'undefined') {
-      init();
-    }
+  $(window).load(function () {
+    initBtnmenu();
+    initAccordion();
   });
 
-  function init() {
+  function initBtnmenu() {
+    var $btn = $('.menu-btn');
 
+
+    $btn.on('click touch', menuOpen);
+
+    function menuOpen(e) {
+      e.preventDefault();
+
+      $('body').toggleClass('open-menu');
+    }
+
+    $('.header-wrapper').prepend('<a href="#" class="close close-bg"></a>');
+    var $close = $('.close');
+
+    $close.on('click touch', function (e) {
+      e.preventDefault();
+
+      $('body').removeClass('open-menu');
+    })
+  }
+
+  function initAccordion() {
+    var $btn = $('.section-faq .title');
+
+    $btn.on('click touch', function (e) {
+      e.preventDefault();
+
+      var $parent = $(this).parent();
+
+      $parent.toggleClass('active');
+    })
   }
 
 })(jQuery);
